ce8ec6313d89496a3d8889f3df33e57ce96e7f8f
dimuthu_l <dimuthu_l@epiclanka.net>
2022-05-28 02:28:27 +0530

    some kotling things

--- app/src/main/java/com/ktl1/basic1/MainActivity.kt
+++ app/src/main/java/com/ktl1/basic1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ktl1.basic1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,17 +17,28 @@ class MainActivity : AppCompatActivity() {
         val message = findViewById<TextView>(R.id.textView)
         val textField = findViewById<EditText>(R.id.editTextTextPersonName)
         val button = findViewById<Button>(R.id.button)
+        val nextButton = findViewById<Button>(R.id.navButton)
+        var data = ""
+        nextButton.visibility = INVISIBLE
+
 
         button.setOnClickListener {
-
             if (textField.text.isEmpty()) {
+                nextButton.visibility = INVISIBLE
                 Toast.makeText(this@MainActivity, "Empty Field!", Toast.LENGTH_LONG).show()
             } else {
+                nextButton.visibility = VISIBLE
                 textField.hint = "fuck"
-                val data = textField.text.toString()
+                data = textField.text.toString()
                 message.text = "Welcome Brother ! $data"
                 textField.text.clear()
             }
+        }
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("data",data)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/ktl1/basic1/SecondActivity.kt
+++ app/src/main/java/com/ktl1/basic1/SecondActivity.kt
@@ -1,11 +1,18 @@
 package com.ktl1.basic1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val text = findViewById<TextView>(R.id.textView2)
+
+        val intent = intent.getStringExtra("data")
+        text.text = intent
     }
 }
