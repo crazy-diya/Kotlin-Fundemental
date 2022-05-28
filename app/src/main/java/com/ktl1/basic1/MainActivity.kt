908fdebee083902b036a8eaa288d0b758a420d8c
dimuthu_l <dimuthu_l@epiclanka.net>
2022-05-28 15:33:11 +0530

    basic

--- app/src/main/java/com/ktl1/basic1/MainActivity.kt
+++ app/src/main/java/com/ktl1/basic1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ktl1.basic1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View.*
 import android.widget.Button
 import android.widget.EditText
@@ -20,9 +21,12 @@ class MainActivity : AppCompatActivity() {
         val nextButton = findViewById<Button>(R.id.navButton)
         var data = ""
         nextButton.visibility = INVISIBLE
+        Log.i("info","Welcome to fucking message area")
 
 
         button.setOnClickListener {
+            Log.e("error","Fucking error")
+            Log.w("warning","Warning area")
             if (textField.text.isEmpty()) {
                 nextButton.visibility = INVISIBLE
                 Toast.makeText(this@MainActivity, "Empty Field!", Toast.LENGTH_LONG).show()
@@ -40,5 +44,30 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("data",data)
             startActivity(intent)
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("override","Main Application OnPause Function")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("override","Main Application OnResume Function")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("override","Main Application OnStop Function")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("override","Main Application OnRestart Function")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("override","Main Application ONDestroy Function")
     }
 }
--- app/src/main/java/com/ktl1/basic1/SecondActivity.kt
+++ app/src/main/java/com/ktl1/basic1/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.ktl1.basic1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
@@ -12,7 +13,34 @@ class SecondActivity : AppCompatActivity() {
 
         val text = findViewById<TextView>(R.id.textView2)
 
+        Log.i("info","Welcome to fucking message area")
         val intent = intent.getStringExtra("data")
         text.text = intent
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("override","Second Application OnPause Function")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("override","Second Application OnResume Function")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("override","Second Application OnStop Function")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("override","Second Application OnRestart Function")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("override","Second Application ONDestroy Function")
     }
 }
